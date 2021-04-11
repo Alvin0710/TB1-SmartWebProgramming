78ecdb38a534e4af26033ad04ce25d8efcaf660a
Alvin Irwanto <irwantoalvin@gmail.com>
2021-04-11 19:05:54 +0700

    edit css

--- doa.php
+++ doa.php
@@ -25,8 +25,6 @@
     </div>
 </class>
 
-<hr class="dashed">
-
 <form class="example" action="/action_page.php" style="margin:auto;max-width:900px">
   <input type="text" placeholder="Search.." name="search2">
   <button type="submit"><i class="fa fa-search"></i></button>
