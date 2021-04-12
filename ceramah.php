22b4f1d671224f8c95092921af66c7c28894f5c3
Alvin Irwanto <irwantoalvin@gmail.com>
2021-04-12 22:52:59 +0700

    add ceramah

--- ceramah.php
+++ ceramah.php
@@ -24,6 +24,38 @@
     </div>
 </class>
     
+<div class="container">
+  <form class="example" action="/action_page.php" style="margin:auto;max-width:900px">
+    <input type="text" placeholder="Search.." name="search2">
+    <button type="submit"><i class="fa fa-search"></i></button>
+  </form>
+</div>
+<br><br>
+
+
+<div class="row ceramah-row-main">
+    <div class="col-lg-5 col-sm-2 col-md-6 ceramah-box">
+        <iframe src="https://www.youtube.com/embed/89XQC7ey8DU"></iframe>
+    </div>
+
+    <div class="col-lg-5 col-sm-2 col-md-6 ceramah-box">
+        <iframe src="https://www.youtube.com/embed/T5KbPoSFaME"></iframe>
+    </div>
+    <div class="col-lg-5 col-sm-2 col-md-6 ceramah-box">
+        <iframe src="https://www.youtube.com/embed/X3_eAId-XaM"></iframe>
+    </div>
+
+    <div class="col-lg-5 col-sm-2 col-md-6 ceramah-box">
+        <iframe src="https://www.youtube.com/embed/JOML0AGrTNU"></iframe>
+    </div>
+    </div>
+</div>
+    
+</div>
+
+<br>
+<br>
+
 </div>
 <?php
 
