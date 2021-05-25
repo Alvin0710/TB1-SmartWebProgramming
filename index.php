a5e9a10877c35c1c09e5073e02b26dc61e7b76fd
Alvin Irwanto <irwantoalvin@gmail.com>
2021-05-25 09:00:13 +0700

    edit image

--- index.php
+++ index.php
@@ -9,13 +9,13 @@ include('header.php');
     <div class="row">
         <a href="quran.php" class="col-lg-4 col-sm-2 col-md-6 col" style="background-color: white;">
             <div>
-            <center><img src="images/quran.jpg" alt="" class="image-menu"></center>
+            <center><img src="images/quran.png" alt="" class="image-menu"></center>
                 <h3 class="main-title">AL-QUR'AN</h3>
             </div>
         </a>
         <a href="doa.php" class="col-lg-4 col-sm-2 col-md-6 col" style="background-color: #f2f2f280">
             <div>
-            <center><img src="images/doa.jpg" alt="" class="image-menu"></center>
+            <center><img src="images/doa.png" alt="" class="image-menu"></center>
                 <h3 class="main-title">
                     DO'A <br>
                     - DO'A
@@ -24,7 +24,7 @@ include('header.php');
         </a>
         <a href="kisah.php" class="col-lg-4 col-sm-2 col-md-6 col" style="background-color: #e6e6e67c;">
             <div>
-            <center><img src="images/kisah.jpg" alt="" class="image-menu"></center>
+            <center><img src="images/kisah.png" alt="" class="image-menu"></center>
                 <h3 class="main-title">
                     KISAH NABI <br>
                     & RASUL
@@ -33,7 +33,7 @@ include('header.php');
         </a>
          <a href="jadwal.php" class="col-lg-4 col-sm-2 col-md-6 col" style="background-color: #cccccc7e">
             <div>
-            <center><img src="images/jadwal.jpg" alt="" class="image-menu"></center>
+            <center><img src="images/jadwal.png" alt="" class="image-menu"></center>
                 <h3 class="main-title">
                     JADWAL <br>
                     SHOLAT
@@ -43,7 +43,7 @@ include('header.php');
 
         <a href="ceramah.php" class="col-lg-4 col-sm-2 col-md-6 col" style="background-color: #b3b3b37a;">
             <div>
-            <center><img src="images/ceramah.jpg" alt="" class="image-menu"></center>
+            <center><img src="images/ceramah.png" alt="" class="image-menu"></center>
                 <h3 class="main-title">
                     CERAMAH
                 </h3>
@@ -52,7 +52,7 @@ include('header.php');
 
         <a href="tebak.php" class="col-lg-4 col-sm-2 col-md-6 col" style="background-color: #9999997c;">
             <div>
-            <center><img src="images/tebak.jpg" alt="" class="image-menu"></center>
+            <center><img src="images/tebak.png" alt="" class="image-menu"></center>
                 <h3 class="main-title">
                     TEBAK AYAT <br>
                     AL-QUR'AN
