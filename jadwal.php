e100c09f9c2592a8cf46ac2d1008081eccbc65e1
Alvin Irwanto <irwantoalvin@gmail.com>
2021-06-05 10:07:27 +0700

    edit

--- jadwal.php
+++ jadwal.php
@@ -8,7 +8,10 @@
 
 <?php
  include('header.php');
- include('back-api.php');
+
+ $json_url = "https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/kota.json";
+  $json = file_get_contents($json_url);
+  $data = json_decode($json);
 ?>
 
 <div class="main-content" id="<?php echo empty($_GET['page'])?'pageContent':''; ?>">
@@ -25,27 +28,50 @@
     </div>
 </class>
 
-<div class="container">
-<form class="example" action="/action_page.php" style="margin:auto;max-width:900px">
+<!-- <form class="example" action="/action_page.php" style="margin:auto;max-width:900px">
   <input type="text" placeholder="Tulis nama kotamu .." name="search2">
   <button type="submit"><i class="fa fa-search"></i></button>
-</form><br><br><br>
+</form><br><br><br> -->
+<div class="container input-icons">
+  <form class="example" action="/action_page.php" style="margin:auto;max-width:900px">
+  <i class="fa fa-search icon" width="30" ></i>
+  <input type="text" placeholder="Di Kota mana kamu tinggal?" name="search" id="myInput" class="input-field" onkeyup="myFunction()" action="/action_page.php">
+  </form>
+</div><br><br>
+
+<div class="row quran-row-main" id="data-jadwal">
+    <?php
+        $i = 0;
+            foreach ($data as $value){
+            ?>
+            <div class="col-lg-3 col-sm-1 col-md-5 quran-box" data-toggle="modal" data-target="#myModal<?php echo $i; ?>">
+                <p class="justify-content-center align-self-center">
+                  <center>
+                    <b style="text-transform: capitalize;" ><?=$value?></b>
+                  </center>
+                </p>
+            </div>
+            
+            <?php
+            $i += 1;
+        }
+    ?>
 </div>
     
-<div class="container">
+<!-- <div class="container">
 <h1 style="text-align:center; text-transform: capitalize;">
   <b>
-   <?php
-      echo $data;
-   ?>
+    Tangerang
   </b>
 </h1>
 
 <br><br>
 <h3 style="text-align:center">Mei 2021</h3>
-<br><br>
+<br><br> -->
 
-<div class="table-responsive">
+<!-- <p style="text-align: center;"><iframe src="https://www.jadwalsholat.org/adzan/monthly.php?id=265" height="940" width="430" frameborder="0"></iframe><a href="https://www.jadwalsholat.org" target="_blank"><img class="aligncenter" style="text-align: center;" alt="jadwal-sholat" src="https://www.jadwalsholat.org/wp-content/uploads/2013/09/jadwal-sholat.png" width="81" height="18" /></a></p> -->
+
+<!-- <div class="table-responsive">
 <table class="table table-hover">
   <thead class="thead-dark">
     <tr>
@@ -140,7 +166,7 @@
     </tr>
 
   </tbody>
-</table>
+</table> -->
 
 </div>
 <br><br>
@@ -151,6 +177,24 @@
 include('footer.php');
 ?>
 
+<script>
+function myFunction() {
+    var input, filter, container, div, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    container = document.getElementById("data-jadwal");
+    div = container.getElementsByTagName("div");
+    for (i = 0; i < div.length; i++) {
+        a = div[i].getElementsByTagName("b")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            div[i].style.display = "";
+        } else {
+            div[i].style.display = "none";
+        }
+    }
+}
+</script>
 
 </body>
 </html>
--- test.php
+++ test.php
@@ -17,19 +17,19 @@
     // }
     
 
-	// $json_url = "https://islamic-api-zhirrr.vercel.app/api/doaharian";
-	// $json = file_get_contents($json_url);
-    // $data = json_decode($json);
-    
-    // foreach ($data->data as $mydata){
-    //     echo $mydata->title;
-    // }
-
-    $json = file_get_contents("data/doaharian.json");
+	$json_url = "https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/kota.json";
+	$json = file_get_contents($json_url);
     $data = json_decode($json);
     
     foreach ($data as $value){
-        echo $value->title;
+        echo $value."<br>";
     }
+
+    // $json = file_get_contents("data/doaharian.json");
+    // $data = json_decode($json);
+    
+    // foreach ($data as $value){
+    //     echo $value->title;
+    // }
 
 ?>
